edd0bb6057ed25f792eaab4779ecc335f34e6220
Serhii Lekhman <namhels@MacBook-Pro-Serhii.local>
2023-11-04 21:00:26 +0100

    add ToggleTheme

--- src/App.jsx
+++ src/App.jsx
@@ -6,20 +6,28 @@ import {
   Heading,
   Spacer,
 } from '@chakra-ui/react';
+import ToggleTheme from 'components/ToggleTheme';
 
 export const App = () => {
   return (
     <div className='backgroundImage'>
       <Flex p={5} minWidth="max-content" alignItems="center" gap="2">
         <Box p="2">
-          <Heading size="md">DW Żuraw</Heading>
+          <Heading size="md">Dom Wczasowy Żuraw</Heading>
         </Box>
         <Spacer />
         <ButtonGroup gap="2">
-          <Button colorScheme="teal">Sign Up</Button>
-          <Button colorScheme="teal">Log in</Button>
+          <Button colorScheme="teal">Turnusy rehabilitacyjne 2021</Button>
+          <Button colorScheme="teal">Zabiegi rehabilitacyjne</Button>
+          <Button colorScheme="teal">O nas</Button>
+          <Button colorScheme="teal">Galeria</Button>
+          <Button colorScheme="teal">Kontakt</Button>
         </ButtonGroup>
+        <Spacer />
+        <ToggleTheme />
       </Flex>
     </ div>
   );
 };
+
+
---
+++ src/components/ToggleTheme.jsx
@@ -0,0 +1,20 @@
+import { FaMoon, FaSun } from 'react-icons/fa';
+import { IconButton, useColorMode } from '@chakra-ui/react';
+
+const ToggleTheme = () => {
+  const { toggleColorMode, colorMode } = useColorMode();
+
+  return (
+    <IconButton
+      colorScheme="green"
+      aria-label="toggle theme"
+      rounded="full"
+      ml={2}
+      size="md"
+      onClick={toggleColorMode}
+      icon={colorMode === 'dark' ? <FaSun /> : <FaMoon />}
+    />
+  );
+};
+
+export default ToggleTheme;
